7210fff92be53ce0d8e7d0baa7635bc8215a3502
Dorian Krause <dorian@91df63fb.com>
2014-11-22 11:19:48 +0100

    Add new devel.h header
    The devel.h header contains static definitions of
    data that is supposingly variable in the final software.
    This header should go away step-by-step as the development
    progresses.

---
+++ devel.h
@@ -0,0 +1,40 @@
+
+#ifndef SPAWN_DEVEL_H_INCLUDED
+#define SPAWN_DEVEL_H_INCLUDED 1
+
+/*
+ * This is a development header that should help during the
+ * development of the program by hardcoding certain data and
+ * logic that should be variable in the final release.
+ *
+ * MAKE SURE THIS HEADER IS GONE BEFORE RELEASING THE SOFTWARE!
+ */
+
+/*
+ * A compressed host list will be the user input. This will be
+ * stored somewhere in the option list. 
+ *
+ * -o Hosts="a[01-10],a15"
+ */
+static const char spawn_devel_hosts[] = "a[01-20],a35"
+
+/*
+ * The expanded host list will be created from spawn_devel_hosts
+ * by a function.
+ */
+static const char *spawn_devel_hostlist[] = {
+	"-",	/* root. Remaining host indices start at one. */
+	"a01", "a02", "a03", "a04", "a05", "a06", "a07", "a08",
+	"a09", "a10", "a11", "a12", "a13", "a14", "a15", "a16",
+	"a17", "a18", "a19", "a20", "a35"
+};
+
+/*
+ * The tree width is read from the command line.
+ *
+ * -o TreeWidth=3 
+ */
+static const int spawn_devel_tree_width = 4;
+
+#endif
+
