fa84aee9a54cf9554f412d89712e55d9ec4a720b
Dorian Krause <dorian@91df63fb.com>
2014-11-22 10:39:20 +0100

    Fix main() function
    This commit adds the logic in main() to branch out to either
    _main_on_local() or _main_on_other() depending on the context
    of the program invocation. We use the last 13 characters of
    argv[0] to decide whether this is a remote invocation (in this
    case the string should end on "libexec/spawn") or the invocation
    of the spawn program itself.

--- main.c
+++ main.c
@@ -1,22 +1,38 @@
 
-#include "config.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "config.h"
 
 static int _main_on_local(int argc, char **argv);
 static int _main_on_other(int argc, char **argv);
 
-static char argv0[SPAWN_MAX_PATH_LEN];
-
 int main(int argc, char **argv)
 {
-	/* Expand argv[0] to full path */	
+	int n, m;
+	int ret;
 
-	/* Call _main_on_local or _main_on_other depending on
-	   argv[0] */
+	/* This is a simple method to recognize whether to call
+	 * _main_on_other or _main_on_local. On the remote side
+	 * we always execute the libexec spawn executable using
+	 * a full path (PATH is empty). If argv[0] ends with
+	 * "libexec/spawn" we assume that _main_on_other is the
+	 * next step. This should be accurate in 99% of the cases
+	 * and in 100% of the cases where the application is
+	 * used as intended.
+	 */
 
-	return _main_on_local(argc, argv);
+	n = strlen(argv[0]);
+	m = sizeof("libexec/spawn") - 1;
+
+	if ((n >= m) && !strcmp(argv[0] + n - m, "libexec/spawn"))
+		ret = _main_on_other(argc, argv);
+	else
+		ret = _main_on_local(argc, argv);
+
+	return 0;
 }
-
 
 static int _main_on_local(int argc, char **argv)
 {
