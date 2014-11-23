45d0c1e2cdfaa125a68b20d3cc8fe5f23662cb1f
Dorian Krause <dorian@91df63fb.com>
2014-11-23 16:16:37 +0100

    Add allocator interface and first implementation
    This commit adds alloc.{h,c} to the repository. alloc.h
    defines the struct alloc base class. An allocator on top
    of the libc functions is provided.

---
+++ alloc.c
@@ -0,0 +1,147 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "config.h"
+#include "compiler.h"
+#include "alloc.h"
+
+
+static int _libc_malloc(struct alloc *self, void **p, ll num, ll sz,
+                        const char* file, const char* func, long line, const char* fmt, ...);
+static int _libc_zalloc(struct alloc *self, void **p, ll num, ll sz,
+                        const char* file, const char* func, long line, const char* fmt, ...);
+static int _libc_realloc(struct alloc *self, void **p, ll onum, ll osz, ll nnum, ll nsz,
+                         const char* file, const char* func, long line, const char* fmt, ...);
+static int _libc_free(struct alloc *self, void **p, ll num, ll sz,
+                      const char* file, const char* func, long line, const char* fmt, ...);
+static int _libc_zfree(struct alloc *self, void **p, ll num, ll sz,
+                       const char* file, const char* func, long line, const char* fmt, ...);
+
+struct libc_alloc
+{
+	struct alloc	base;
+	int		debug;
+};
+
+static struct alloc_ops _libc_alloc_ops = {
+	.malloc = _libc_malloc,
+	.zalloc = _libc_zalloc,
+	.realloc = _libc_realloc,
+	.free = _libc_free,
+	.zfree = _libc_zfree
+};
+
+static struct libc_alloc _libc_alloc_no_debug = {
+	.base  = {
+		.ops = &_libc_alloc_ops
+	},
+	.debug = 0
+};
+
+static struct libc_alloc _libc_alloc_wi_debug = {
+	.base  = {
+		.ops = &_libc_alloc_ops
+	},
+	.debug = 1
+};
+
+
+struct alloc *libc_allocator()
+{
+	return (struct alloc *)&_libc_alloc_no_debug;
+}
+
+struct alloc *libc_allocator_with_debugging()
+{
+	return (struct alloc *)&_libc_alloc_wi_debug;
+}
+
+
+static int _libc_malloc(struct alloc *self, void **p, ll num, ll sz,
+                        const char* file, const char* func, long line, const char* fmt, ...)
+{
+	if (unlikely(!self || !p || (num < 0) || (sz < 0)))
+		return -EINVAL;
+
+	*p = malloc(num*sz);
+
+	if (unlikely(((struct libc_alloc *)self)->debug)) {	/* optimize for non-debug path */
+		/* TODO */
+	}
+
+	if (unlikely(!(*p)))
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int _libc_zalloc(struct alloc *self, void **p, ll num, ll sz,
+                        const char* file, const char* func, long line, const char* fmt, ...)
+{
+	if (unlikely(!self || !p || (num < 0) || (sz < 0)))
+		return -EINVAL;
+
+	*p = calloc(num, sz);
+
+	if (unlikely(((struct libc_alloc *)self)->debug)) {	/* optimize for non-debug path */
+		/* TODO */
+	}
+	
+	if (unlikely(!(*p)))
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int _libc_realloc(struct alloc *self, void **p, ll onum, ll osz, ll nnum, ll nsz,
+                         const char* file, const char* func, long line, const char* fmt, ...)
+{
+	if (unlikely(!self || !p || (onum < 0) || (osz < 0) || (nnum < 0) || (nsz < 0)))
+		return -EINVAL;
+	
+	*p = realloc(*p, nnum*nsz);
+
+	if (unlikely(((struct libc_alloc *)self)->debug)) {	/* optimize for non-debug path */
+		/* TODO */
+	}
+	
+	if (unlikely(!(*p)))
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int _libc_free(struct alloc *self, void **p, ll num, ll sz,
+                      const char* file, const char* func, long line, const char* fmt, ...)
+{
+	if (unlikely(!self || !p || (num < 0) || (sz < 0)))
+		return -EINVAL;
+	
+	if (unlikely(((struct libc_alloc *)self)->debug)) {	/* optimize for non-debug path */
+		/* TODO */
+	}
+
+	free(*p);	
+	*p = NULL;
+
+	return 0;
+}
+
+static int _libc_zfree(struct alloc *self, void **p, ll num, ll sz,
+                       const char* file, const char* func, long line, const char* fmt, ...)
+{
+	if (unlikely(!self || !p || (num < 0) || (sz < 0)))
+		return -EINVAL;
+	
+	if (unlikely(((struct libc_alloc *)self)->debug)) {	/* optimize for non-debug path */
+		/* TODO */
+	}
+
+	free(memset(*p, 0, num*sz));
+	*p = NULL;
+
+	return 0;
+}
+
---
+++ alloc.h
@@ -0,0 +1,81 @@
+
+#ifndef SPAWN_ALLOC_H_INCLUDED
+#define SPAWN_ALLOC_H_INCLUDED 1
+
+#include "ints.h"
+
+struct alloc;
+struct alloc_ops;
+
+/*
+ * Allocator structure. Implementations will usually wrap this structure and set a custom
+ * alloc_ops pointer.
+ */
+struct alloc
+{
+	struct alloc_ops *ops;
+};
+
+/*
+ * Allocator operations. All ("heavy") functions take information about the location of
+ * the invocation to aid debugging.
+ */
+struct alloc_ops
+{
+	/* Allocate num elements of size sz each. The result will be a buffer of size (at least) num*sz.
+	 */
+	int	(*malloc )(struct alloc *self, void **p, ll num, ll sz, 
+		           const char* file, const char* func, long line, const char* fmt, ...);
+	/* Same as malloc except that the resulting buffer is guaranteed to be zeroed. Note that this means
+	 * that the corresponding pages are touched by the allocating thread which may impact the memory
+	 * placmement on NUMA machines (first touch policy).
+	 */
+	int	(*zalloc )(struct alloc *self, void **p, ll num, ll sz, 
+		           const char* file, const char* func, long line, const char* fmt, ...);
+	/* Reallocate a buffer that was allocated with malloc or zalloc passing onum and osz such that the
+	 * new buffer has place for nnum elements of size nsz each.
+	 */
+	int	(*realloc)(struct alloc *self, void **p, ll onum, ll osz, ll nnum, ll nsz,
+		           const char* file, const char* func, long line, const char* fmt, ...);
+	/* 
+	 */
+	int	(*free)(struct alloc *self, void **p, ll num, ll size, 
+	                const char* file, const char* func, long line, const char* fmt, ...);
+	/* Same as free but clears memory content before freeing the allocation.
+	 */
+	int	(*zfree)(struct alloc *self, void **p, ll num, ll size, 
+	                 const char* file, const char* func, long line, const char* fmt, ...);
+};
+
+#define	MALLOC(SELF, P, NUM, SZ, FMT, ...) \
+	self->ops->malloc((SELF), (P), (NUM), (SZ), __FILE__, __func__, __LINE__, FMT, ## __VA_ARGS__)
+#define	ZALLOC(SELF, P, NUM, SZ, FMT, ...) \
+	self->ops->zalloc((SELF), (P), (NUM), (SZ), __FILE__, __func__, __LINE__, FMT, ## __VA_ARGS__)
+#define REALLOC(SELF, P, ONUM, OSZ, NNUM, NSZ, FMT, ...) \
+	self->ops->realloc((SELF), (P), (ONUM), (NNUM), (SZ), __FILE__, __func__, __LINE__, FMT, ## __VA_ARGS__)
+#define FREE(SELF, P, NUM, SZ, FMT, ...) \
+	self->ops->free((SELF), (P), (NUM), (SZ), __FILE__, __func__, __LINE__, FMT, ## __VA_ARGS__)
+#define ZFREE(SELF, P, NUM, SZ, FMT, ...) \
+	self->ops->zfree((SELF), (P), (NUM), (SZ), __FILE__, __func__, __LINE__, FMT, ## __VA_ARGS__)
+
+
+/*
+ * get a pointer to an allocator that works directly on top of the malloc/calloc/free functions provided by libc.
+ * Even though this function may return different alloc instances when called multiple times all these allocators
+ * use the same pool size.
+ * The allocator will not make use of the debug information provided.
+ */
+struct alloc *libc_allocator();
+
+/*
+ * Get a pointer to an allocator that works directly on top of malloc/calloc/free - just like libc_allocator() - but
+ * additionally provides debugging information. Each operation will result in an info() message send to stderr and thus
+ * may slowdown the execution significantly.
+ *
+ * TODO Provide an option for a ring buffer that can be written to for debugging purpose.
+ *
+ */
+struct alloc *libc_allocator_with_debugging();
+
+#endif
+
