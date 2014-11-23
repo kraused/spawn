2dc3a9fd10daefa04a5155044d39ea5953a78189
Dorian Krause <dorian@91df63fb.com>
2014-11-23 18:15:43 +0100

    ints.h: Add new integer types

--- ints.h
+++ ints.h
@@ -2,7 +2,22 @@
 #ifndef SPAWN_INTS_H_INCLUDED
 #define SPAWN_INTS_H_INCLUDED 1
 
-typedef long long		ll;
+#include <stdint.h>
+
+/* shorter names for the standard types and without this
+ * annoying _t appened. We spell out the signedness of the
+ * type.
+ */
+typedef int8_t  	si8;
+typedef uint8_t 	ui8;
+typedef int16_t 	si16;
+typedef uint16_t	ui16;
+typedef int32_t 	si32;
+typedef uint32_t	ui32;
+typedef int64_t 	si64;
+typedef uint64_t	ui64;
+
+typedef long long         	ll;
 typedef unsigned long long	ull;
 
 #endif
