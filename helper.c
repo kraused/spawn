a5da11d33b4fdf8e0918278ebe2f0ed0977340b0
Dorian Krause <dorian@91df63fb.com>
2014-11-29 17:13:31 +0100

    Merge xstring.{h,c} and helper.{h,c}
    This commit moves the content of the xstring.{h,c} files to
    helper.{h,c}.

--- helper.c
+++ helper.c
@@ -28,14 +28,14 @@ int do_close(int fd)
 			if (likely(EINTR == errno))
 				continue;
 
-			error("close() failed. errno = %d says '%s'.", 
+			error("close() failed. errno = %d says '%s'.",
 			      errno, strerror(errno));
 			return -1;
 		}
 
 		break;
 	}
-	
+
 	return 0;
 }
 
@@ -44,10 +44,12 @@ int do_accept(int sockfd, struct sockaddr *addr, ll *addrlen)
 	int fd;
 	socklen_t len = 0;
 
-	if (unlikely(!addrlen))
+	/* addr and addrlen can be null but not both at the same time. */
+	if ((!addr && addrlen) || (addr && !addrlen))
 		return -EINVAL;
 
-	len = *addrlen;
+	if (addrlen)
+		len = *addrlen;
 
 	while (1) {
 		fd = accept(sockfd, addr, &len);
@@ -55,16 +57,82 @@ int do_accept(int sockfd, struct sockaddr *addr, ll *addrlen)
 			if (likely(EINTR == errno || EAGAIN == errno))
 				continue;
 
-			error("accept() failed. errno = %d says '%s'.", 
+			error("accept() failed. errno = %d says '%s'.",
 			      errno, strerror(errno));
-			return -1;
+			return -errno;
 		}
 
 		break;
 	}
 
-	*addrlen = len;
+	if (addrlen)
+		*addrlen = len;
 
 	return fd;
+}
+
+int xstrdup(struct alloc *alloc, const char *istr, char **ostr)
+{
+	int err;
+	int n;
+
+	if (unlikely(!istr || !ostr))
+		return -EINVAL;
+
+	n = strlen(istr) + 1;
+
+	err = ZALLOC(alloc, (void **)ostr, n, sizeof(char), "xstrdup");
+	if (unlikely(err)) {
+		error("ZALLOC() failed with error %d.", err);
+		return err;
+	}
+
+	memcpy(*ostr, istr, n);
+
+	return 0;
+}
+
+int array_of_str_dup(struct alloc *alloc, int n, const char **istr,
+                     char ***ostr)
+{
+	int err, tmp;
+	int i;
+
+	if (unlikely(!istr || !ostr))
+		return -EINVAL;
+
+	err = ZALLOC(alloc, (void **)ostr, n, sizeof(char *), "xstrduplist");
+	if (unlikely(err)) {
+		error("ZALLOC() failed with error %d.", err);
+		return err;
+	}
+
+	for (i = 0; i < n; ++i) {
+		err = xstrdup(alloc, istr[i], &(*ostr)[i]);
+		if (unlikely(err))
+			goto fail;
+	}
+
+	return 0;
+
+fail:
+	for (i = 0; i < n; ++i) {
+		if (!(*ostr)[i])
+			break;
+
+		/* Do not overwrite err at this point! */
+		tmp = FREE(alloc, (void **)&(*ostr)[i], strlen((*ostr)[i]) + 1,
+		           sizeof(char), "");
+		if (unlikely(tmp))
+			error("FREE() failed with error %d.", tmp);
+	}
+
+	return err;
+}
+
+int array_of_str_free(struct alloc *alloc, int n, char ***str)
+{
+	/* FIXME Not implemented. */
+	return -ENOTIMPL;
 }
 
--- helper.h
+++ helper.h
@@ -4,13 +4,38 @@
 
 #include "ints.h"
 
+struct sockaddr;
 
 #define MAX(X,Y)	(((X) >= (Y)) ? (X) : (Y))
 #define MIN(X,Y)	(((X) >= (Y)) ? (Y) : (X))
 
+#define ARRAYLEN(A)	((sizeof((A)))/(sizeof((A)[0])))
 
+/*
+ * Wrapper around close() that handles EINTR.
+ */
 int do_close(int fd);
+
+/*
+ * Wrapper around accept() that handles EINTR.
+ */
 int do_accept(int sockfd, struct sockaddr *addr, ll *addrlen);
+
+/*
+ * Our version of xstrup which uses the allocator alloc.
+ */
+int xstrdup(struct alloc *alloc, const char *istr, char **ostr);
+
+/*
+ * Duplicate an array of strings.
+ */
+int array_of_str_dup(struct alloc *alloc, int n, const char **istr,
+                     char ***ostr);
+
+/*
+ * Free an array of strings.
+ */
+int array_of_str_free(struct alloc *alloc, int n, char ***str);
 
 #endif
 
