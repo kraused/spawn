712dbdbb0c848b23a317ff6c6cac99c3c254fcef
Dorian Krause <dorian@91df63fb.com>
2014-11-29 17:07:14 +0100

    Add files queue.{h,c}
    In queue.{h,c} a generic queue is implemented.

---
+++ queue.c
@@ -0,0 +1,110 @@
+
+#include <string.h>
+
+#include "config.h"
+#include "compiler.h"
+#include "error.h"
+#include "alloc.h"
+#include "queue.h"
+
+
+int queue_ctor(struct queue *self, struct alloc *alloc, ll capacity)
+{
+	int err;
+
+	if (unlikely(!self || !alloc || capacity < 1))
+		return -EINVAL;
+
+	memset(self, 0, sizeof(*self));
+
+	err = ZALLOC(alloc, (void **)&self->buf, capacity, 
+	             sizeof(void *), "queue");
+	if (unlikely(err)) {
+		error("ZALLOC() failed with error %d.", err);
+		return err;
+	}
+
+	self->alloc    = alloc;
+	self->capacity = capacity;
+	self->head     = 0;
+	self->tail     = 0;
+
+	return 0;
+}
+
+int queue_dtor(struct queue *self)
+{
+	int err;
+
+	err = ZFREE(self->alloc, (void **)&self->buf, self->capacity,
+	            sizeof(void *), "queue");
+	if (unlikely(err)) {
+		error("ZFREE() failed with error %d.", err);
+		return err;
+	}
+
+	memset(self, 0, sizeof(*self));
+
+	return 0;
+}
+
+int queue_change_capacity(struct queue *self, ll capacity)
+{
+	int err;
+	void **buf;
+	ll i;
+
+	if (unlikely(capacity < self->size))
+		return -EINVAL;
+
+	/* We do not use REALLOC here but rather ZALLOC() and ZFREE()
+	 * since the useful data may be split into two seperate groups
+	 * in the buffer and it is easier to copy the data out-of-place. */
+
+	err = ZALLOC(self->alloc, (void **)&buf, capacity, 
+	             sizeof(void *), "queue");
+	if (unlikely(err)) {
+		error("ZALLOC() failed with error %d.", err);
+		return err;
+	}
+
+	for (i = 0; i < self->size; ++i)
+		buf[i] = self->buf[(self->head + i) % self->capacity];
+
+	err = ZFREE(self->alloc, (void **)&self->buf, self->capacity,
+	            sizeof(void *), "queue");
+	if (unlikely(err)) {
+		error("ZFREE() failed with error %d.", err);
+		return err;
+	}
+
+	self->buf      = buf;
+	self->capacity = capacity;
+	self->head     = 0;
+	self->tail     = self->size;
+
+	return 0;
+}
+
+int queue_enqueue(struct queue *self, void *p)
+{
+	if (unlikely(self->capacity == self->size))
+		return -ENOMEM;
+
+	self->buf[(self->tail++) % self->capacity] = p;
+	++self->size;
+
+	return 0;
+}
+
+int queue_dequeue(struct queue *self, void **p)
+{
+	if (unlikely(0 == self->size))
+		return -ENOENT;
+
+	*p = self->buf[(self->head++) % self->capacity];
+	--self->size;
+
+	return 0;
+}
+
---
+++ queue.h
@@ -0,0 +1,60 @@
+
+#ifndef SPAWN_QUEUE_H_INCLUDED
+#define SPAWN_QUEUE_H_INCLUDED 1
+
+/*
+ * A generic queue storing pointers to something. The size of
+ * the queue is fixed but the queue can be resized.
+ * 
+ * This structure is not thread-safe.  
+ */
+struct queue
+{
+	struct alloc	*alloc;
+
+	ll		capacity;
+	void		**buf;
+
+	ll		head;
+	ll		tail;
+	ll		size;	/* Size stored for faster access */
+};
+
+/*
+ * Construct a new queue.
+ */
+int queue_ctor(struct queue *self, struct alloc *alloc, ll capacity);
+
+/*
+ * Destructor for a queue instance.
+ */
+int queue_dtor(struct queue *self);
+
+/*
+ * Increase or decrease the capacity of the queue. It is an error
+ * to decrease the capacity below the current size of the queue.
+ */
+int queue_change_capacity(struct queue *self, ll capacity);
+
+/*
+ * Queue the size of the queue.
+ */
+static inline int queue_size(struct queue *self, ll *size)
+{
+	*size = self->size;
+
+	return 0;
+}
+
+/*
+ * Enqueue p.
+ */
+int queue_enqueue(struct queue *self, void *p);
+
+/*
+ * Dequeue an element from the queue.
+ */
+int queue_dequeue(struct queue *self, void **p);
+
+#endif
+
